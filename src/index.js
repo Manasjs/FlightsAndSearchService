a60fbd4d45a2ee1ba4156096b7dc672236a3ba6c
ManasPW <manaskumarshaw9@gmail.com>
2025-06-02 12:00:53 +0530

    setup basic body_perser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const {PORT}=require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 
 const setAndstartServer=async() => {
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+
     app.listen(PORT,()=>{
     console.log(`app is listning at ${PORT}`);
+
 });
 
 };
